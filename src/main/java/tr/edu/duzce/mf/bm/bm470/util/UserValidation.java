8276685c63da1f0661330a821ec11500d44a1e15
TalhaPekdemir <75481622+TalhaPekdemir@users.noreply.github.com>
2023-06-19 22:37:31 +0300

    register.jsp nin tüm alanlarını kapsayacak şekilde validasyon eklendi

--- src/main/java/tr/edu/duzce/mf/bm/bm470/util/UserValidation.java
+++ src/main/java/tr/edu/duzce/mf/bm/bm470/util/UserValidation.java
@@ -1,10 +1,24 @@
 package tr.edu.duzce.mf.bm.bm470.util;
 
+import java.util.regex.Pattern;
+
 public class UserValidation {
+
+    // RFC 5322 for Email Validation
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
     public static boolean isUsernameValid(String username){
-        if(username.length() < 3 && username.length() > 40){
-            return false;
-        }
-        return true;
+        return username.length() >= 3 && username.length() <= 40;
+    }
+
+    public static boolean isPasswordValid(String password){
+        return password.length() >= 8;
+    }
+
+    public static boolean isEmailValid(String email){
+        return Pattern.compile(EMAIL_REGEX).matcher(email).matches();
+    }
+
+    public static boolean doesPasswordsMatch(String pass1, String pass2){
+        return pass1.equals(pass2);
     }
 }
