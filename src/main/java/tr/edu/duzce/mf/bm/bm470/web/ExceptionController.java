120b6a69580300dbd30a32dc09bee5a9593def82
TalhaPekdemir <75481622+TalhaPekdemir@users.noreply.github.com>
2023-06-21 17:03:53 +0300

    kaldırılacak olan metot kaldırıldı

--- src/main/java/tr/edu/duzce/mf/bm/bm470/web/ExceptionController.java
+++ src/main/java/tr/edu/duzce/mf/bm/bm470/web/ExceptionController.java
@@ -15,22 +15,13 @@ public class ExceptionController {
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public String handleNotFound(HttpServletRequest request, Exception e) {
 
-        System.err.println(request.getRequestURL() + " istegine karsilik karsilayici bulunamadi. " +
-                "Hata mesaji: " + e.getMessage());
         return "error";
     }
 
-//    @ExceptionHandler
-//    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-//    public String handleBadRequest(HttpServletRequest request, Exception e, Model model) {
-//        model.addAttribute("message", "BAD BAD REQUEEEST!");
-//        return "error";
-//    }
     @ExceptionHandler(value = Exception.class)
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleInternalServerError(HttpServletRequest request, Exception e, Model model) {
-        System.err.println(request.getRequestURL() + " istegine karsilik karsilayici bulunamadi. " +
-                "Hata mesaji: " + e.getMessage());
+
         model.addAttribute("message", "Hay aksi! Stajyerimiz yine bir şeyleri bozdu anlaşılan. Birazdan döneriz.");
         return "error";
     }
